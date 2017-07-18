dbe2436d64a03524dbc57de5d4e089d1de5847bb
junyw <wangjunyi13@gmail.com>
2017-07-18 14:18:01 -0400

    Remove gcd code

--- multilevelUITableView/ViewController.swift
+++ multilevelUITableView/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
     let item1 = Item(id: 0, indent: 0, title: "title 0", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", descendants: [1])
     let item2 = Item(id: 1, indent: 1, title: "title 1", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", descendants: [2])
     let item3 = Item(id: 2, indent: 2, title: "title 2", text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore", descendants: [])
-    
+    let item4 = Item(id: 3, indent: 0, title: "title 3", text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore", descendants: [4])
+    let item5 = Item(id: 4, indent: 2, title: "title 4", text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore", descendants: [])
+
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -24,6 +26,8 @@ class ViewController: UIViewController {
         dict.addItemToLast(item1, id: item1.id)
         dict.addItemToLast(item2, id: item2.id)
         dict.addItemToLast(item3, id: item3.id)
+        dict.addItemToLast(item4, id: item4.id)
+        dict.addItemToLast(item5, id: item5.id)
 
         let cellNib = UINib(nibName: "CustomCell", bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: "CustomCell")
@@ -78,7 +82,6 @@ extension ViewController: UITableViewDelegate {
         } else {
             cell.detailLabel.text = ""
             cell.layoutSubviews()
-
             self.collapseRows(tableView, descendantsOf: indexPath.row)
         }
     }
@@ -91,21 +94,15 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "CustomCell"
         let cell: CustomCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CustomCell
-        
-        let queue = DispatchQueue.global()
+        cell.layoutSubviews()
 
-        queue.async {
-            let item = self.dict.getItem(atRow: indexPath.row)
-            print("\(item)")
-            DispatchQueue.main.async {
-                cell.titleLabel.text = item?.title
-                cell.detailLabel.text = item?.text
-                if let indent = item?.indent {
-                    print(indent)
-                    cell.indentationLevel = indent
-                    cell.contentView.backgroundColor = UIColor.black.withAlphaComponent(CGFloat(indent)/50.0)
-                }
-            }
+        let item = self.dict.getItem(atRow: indexPath.row)
+        cell.titleLabel.text = item?.title
+        cell.detailLabel.text = item?.text
+        if let indent = item?.indent {
+            print(indent)
+            cell.indentationLevel = indent
+            cell.contentView.backgroundColor = UIColor.black.withAlphaComponent(CGFloat(indent)/50.0)
         }
         
         return cell
